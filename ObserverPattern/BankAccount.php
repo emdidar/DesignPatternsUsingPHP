e7f414fbf52b37a4217868d388420f3ba87da0fc
Mohammad Didarul Alam <emdidar@gmail.com>
2022-01-28 14:06:46 +0600

    Create BankAccount.php

---
+++ ObserverPattern/BankAccount.php
@@ -0,0 +1,24 @@
+<?php
+
+class BankAccount implements Observable{
+    private $observers=[];
+    function attach(Observer $observer)
+    {
+        $this->observers[] = $observer;
+    }
+    function notify($info, $event)
+    {
+        foreach($this->observers as $observer)
+        {
+            $observer->update($this, $info, $event);
+        }
+    }
+    function withdraw($money)
+    {
+        $this->notify($money, 'MONEY_WITHDRAW');
+    }
+    function deposit($money)
+    {
+        $this->notify($money, 'MONEY_DEPOSIT');
+    }
+}
